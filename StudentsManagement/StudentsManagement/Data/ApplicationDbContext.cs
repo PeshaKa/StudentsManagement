11e70ea2396aa22bc7df63ef2116267c207a8db7
Pesha <pesha.enock@swizzsoft.com>
2025-01-26 12:26:02 +0300

    Login update

--- StudentsManagement/StudentsManagement/Data/ApplicationDbContext.cs
+++ StudentsManagement/StudentsManagement/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using StudentsManagement.Model;
 
 namespace StudentsManagement.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+
+        public DbSet<Student> Students { get; set; }
     }
 }
